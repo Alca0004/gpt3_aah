07f4670dbbd7e11ce1a19c4a35ea07deaf26fcfb
Aaron Alcala <alca0004@gmail.com>
2023-07-01 11:54:17 -0400

    navbar.css completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -38,6 +38,10 @@ const Navbar = () => {
                     <div className='gpt3__navbar-menu_container scale-up-center'>
                         <div className='gpt3__navbar-menu_container_links'>
                             <Menu />
+                            <div className='gpt3__navbar-menu_container-links-sign'>
+                                <p>Sign In</p>
+                                <button type="button">Sign Up</button>
+                            </div>
                         </div>
                     </div>
                 )};
